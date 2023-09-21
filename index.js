1df609d55bc45934ac4b660909e675f2981ddc72
johnny <john.robertson88@yahoo.com>
2023-09-21 00:40:03 -0700

    Establish a basic ejs page which displays an h1 message on an ejs template

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ import bodyParser from "body-parser"
 const app = express()
 const port = 3000
 
+/**
+ * Get root to establish a home page
+ */
 app.get("/", (req, res) => {
+    let hMessage = "Get a Drink!"
+    res.render("index.ejs", {message: hMessage})
+
 })
 
+/**
+ * Create a listener on a give port to run the application
+ */
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
